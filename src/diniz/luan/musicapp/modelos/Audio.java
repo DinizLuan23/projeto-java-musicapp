be633364e484b0d1f5bc7dc71a1470aaaca86dc3
Luan Diniz <luan.diniz23@hotmail.com>
2023-10-27 13:59:49 -0300

    Classes principal, musica, podcast e audio

---
+++ src/diniz/luan/musicapp/modelos/Audio.java
@@ -0,0 +1,36 @@
+package diniz.luan.musicapp.modelos;
+
+public class Audio {
+    private String titulo;
+    private int totalReproducoes;
+    private int totalCurtidas;
+    private int classificacao;
+
+    public void curtir() {
+        totalCurtidas ++;
+    }
+
+    public void reproduzir() {
+        totalReproducoes++;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public int getTotalReproducoes() {
+        return totalReproducoes;
+    }
+
+    public int getTotalCurtidas() {
+        return totalCurtidas;
+    }
+
+    public int getClassificacao() {
+        return classificacao;
+    }
+}
